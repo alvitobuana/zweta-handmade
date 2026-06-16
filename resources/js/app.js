c75dc37265e7e8665568ef111aed7bbe5ce4495f
RaafiRavarizkie103012300289 <raafiravarizkie@student.telkomuniversity.com>
2026-06-16 18:30:10 +0700

    Initial commit of compro project

---
+++ resources/js/app.js
@@ -0,0 +1,3 @@
+import './bootstrap';
+
+// Add your JavaScript imports here.
---
+++ resources/js/bootstrap.js
@@ -0,0 +1,8 @@
+// resources/js/bootstrap.js
+
+// Import dependencies here, for example Axios or Laravel Echo.
+// This file is intentionally minimal to satisfy Vite entry bootstrap imports.
+
+// Example:
+// import axios from 'axios';
+// window.axios = axios;
